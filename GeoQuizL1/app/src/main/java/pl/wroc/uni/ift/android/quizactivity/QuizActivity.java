9d121feaaa3678129dabdaa6431492320333a710
Piotr <p.wolski1000@gmail.com>
2017-10-24 23:36:41 +0200

    lista1, zad. 4 i lista 2

--- GeoQuizL1/app/src/main/java/pl/wroc/uni/ift/android/quizactivity/QuizActivity.java
+++ GeoQuizL1/app/src/main/java/pl/wroc/uni/ift/android/quizactivity/QuizActivity.java
@@ -5,15 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class QuizActivity extends AppCompatActivity {
 
+
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
-    private Button mBackButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviouButton;
 
     private TextView mQuestionTextView;
 
@@ -24,12 +29,18 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_wisla, true)
     };
 
+    private boolean isClicked[] = new boolean[]{
+            true, true, true, true
+    };
+
     private int mCurrentIndex = 0;
+    private int mScore = 0;
 
     //    Bundles are generally used for passing data between various Android activities.
     //    It depends on you what type of values you want to pass, but bundles can hold all
     //    types of values and pass them to the new activity.
     //    see: https://stackoverflow.com/questions/4999991/what-is-a-bundle-in-an-android-application
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +52,10 @@ public class QuizActivity extends AppCompatActivity {
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionsBank.length;
+                mCurrentIndex++;
+                if (mCurrentIndex == mQuestionsBank.length){
+                    mCurrentIndex = 0;
+                }
                 updateQuestion();
             }
         });
@@ -52,6 +66,8 @@ public class QuizActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         checkAnswer(true);
+                        mTrueButton.setEnabled(isClicked[mCurrentIndex]);
+                        isClicked();
                     }
                 }
         );
@@ -61,32 +77,58 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
+                mFalseButton.setEnabled(isClicked[mCurrentIndex]);
+                isClicked();
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionsBank.length;
+//                if(!isClicked[mCurrentIndex-1]){
+//                    mNextButton.setEnabled(false);
+//                }else{
+//                    mNextButton.setEnabled(true);
+//                }
+
+                mCurrentIndex++;
+                if (mCurrentIndex == mQuestionsBank.length){
+                    mCurrentIndex = mQuestionsBank.length-1;
+                    summaryAlert();
+                    disableNavigationButton();
+
+                }
+
                 updateQuestion();
+                mTrueButton.setEnabled(isClicked[mCurrentIndex]);
+                mFalseButton.setEnabled(isClicked[mCurrentIndex]);
             }
         });
 
-        mBackButton = (Button) findViewById(R.id.back_button);
-        mBackButton.setOnClickListener(new View.OnClickListener() {
+        mPreviouButton = (ImageButton) findViewById(R.id.previous_button);
+        mPreviouButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionsBank.length;
+            public void onClick(View view){
+                mCurrentIndex--;
+                if (mCurrentIndex == -1) {
+                    mCurrentIndex = 0;
+                    canNotBackAlert();
+                }
                 updateQuestion();
+                mTrueButton.setEnabled(isClicked[mCurrentIndex]);
+                mFalseButton.setEnabled(isClicked[mCurrentIndex]);
+
             }
         });
         updateQuestion();
     }
+
     private void updateQuestion() {
         int question = mQuestionsBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
+
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionsBank[mCurrentIndex].isAnswerTrue();
 
@@ -94,11 +136,59 @@ public class QuizActivity extends AppCompatActivity {
 
         if (userPressedTrue == answerIsTrue) {
             toastMessageId = R.string.correct_toast;
+            mScore++;
         } else {
             toastMessageId = R.string.incorrect_toast;
         }
-        Toast myToast = Toast.makeText(this, toastMessageId, Toast.LENGTH_SHORT);
-        myToast.setGravity(Gravity.TOP,0,0);
-        myToast.show();
+
+        Toast toast = Toast.makeText(this, toastMessageId, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+    }
+
+
+    private void isClicked(){
+        isClicked[mCurrentIndex] = false;
+        mTrueButton.setEnabled(isClicked[mCurrentIndex]);
+        mFalseButton.setEnabled(isClicked[mCurrentIndex]);
+
+    }
+
+    private void disableNavigationButton(){
+        mNextButton.setEnabled(false);
+        mPreviouButton.setEnabled(false);
+    }
+
+    private void canNotBackAlert(){
+        AlertDialog alert = new AlertDialog.Builder(QuizActivity.this).create();
+        alert.setTitle("Uwaga");
+        alert.setMessage("Nie można się cofnąć!");
+        alert.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.show();
+    }
+
+    private void summaryAlert(){
+        AlertDialog alert = new AlertDialog.Builder(QuizActivity.this).create();
+        alert.setTitle("Koniec pytań");
+        alert.setMessage("Odpowiedziałeś poprawnie na: " + mScore + '/' + mQuestionsBank.length);
+        alert.setButton(AlertDialog.BUTTON_NEGATIVE, "Zagraj ponownie", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                QuizActivity.super.recreate();
+                Toast.makeText(getApplicationContext(), "Zagraj ponownie", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        alert.setButton(AlertDialog.BUTTON_POSITIVE, "Wyjdź", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                QuizActivity.this.finish();
+                Toast.makeText(getApplicationContext(), "Finish", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        alert.show();
     }
 }
